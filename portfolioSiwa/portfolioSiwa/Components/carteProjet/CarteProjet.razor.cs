c326571a17bfb2bd1c7625d6c80fbbc4fe7a7569
Siwa12100 <jean.marcillac12@gmail.com>
2024-04-07 10:13:53 +0200

    travail sur les projets

---
+++ portfolioSiwa/portfolioSiwa/Components/carteProjet/CarteProjet.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace portfolioSiwa.Components.carteProjet
+{
+    public partial class CarteProjet
+    {
+
+        [Parameter]
+        public String cheminImage {  get; set; }
+
+        [Parameter]
+        public String titre {  get; set; }
+
+        [Parameter]
+        public String paragraphe { get; set; }
+
+        [Parameter]
+        public String texteTag { get; set; }
+
+
+        public void CardCliquee()
+        {
+
+        }
+    }
+}
