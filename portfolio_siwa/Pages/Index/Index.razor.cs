038f4ca7b472a6f7f0528f5277c9033006067357
Siwa12100 <jean.marcillac12@gmail.com>
2025-02-24 17:04:04 +0000

    suppression de l'ancienne icône favicon, ajout d'une nouvelle icône GIF, mise à jour du style des composants et ajout de descriptions pour le projet GenseSense dans la page d'accueil

--- portfolio_siwa/Pages/Index/Index.razor.cs
+++ portfolio_siwa/Pages/Index/Index.razor.cs
@@ -5,6 +5,25 @@ namespace portfolio_siwa.Pages.Index
 {
     public partial class Index
     {
+        protected string? TitreProjetGenseSense;
+        protected MarkupString? DescriptionProjetGenseSense;
+        protected string? GeneSenseDescriptionPython;
+        protected string? GeneSenseDescriptionFlask;
+        protected string? GeneSenseDescriptionBlazor;
+        protected string? GeneSenseDescriptionDocker;
+        protected string? GeneSenseDescriptionYolo;
+        protected string? GenseSenseGithub;
 
+        protected override void OnInitialized()
+        {
+            TitreProjetGenseSense = "GenseSense IA";
+            DescriptionProjetGenseSense = new MarkupString("GenseSense est un projet ayant consisté à développer deux intelligences artificielles. La première détecte le genre d'une personne à partir d'une image, tandis que la seconde reconnaît certaines personnes de ma promotion de BUT. <br> En complément, une IA dédiée à la segmentation des visages facilite la classification par genre et par personnes. Enfin, un panel web a été développé pour permettre une interaction simple et directe avec le système.");
+            this.GeneSenseDescriptionPython = "Langage utilisé pour développer les modèles d'IA et traiter les images.";
+            this.GeneSenseDescriptionFlask = "Framework employé pour créer l'API Web qui communique entre les IA et le panel web.";
+            this.GeneSenseDescriptionBlazor = "Plateforme utilisée pour concevoir le panel web interactif. Elle offre une communication en temps réel entre le client et le serveur.";
+            this.GeneSenseDescriptionDocker = "Utilisation pour le déploiement de l'ensemble du projet dans un environnement homogène.";
+            this.GeneSenseDescriptionYolo = "Bibliothèque utilisée pour détecter et segmenter les visages dans les images. Elle fournit des performances rapides et précises pour l'extraction des zones d'intérêt.";
+
+        }
     }
 }
